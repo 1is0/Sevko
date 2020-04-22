bf4e4de474a5b343a53882ac71cb64ce31106dd8
polinasevko <61377437+polinasevko@users.noreply.github.com>
2020-04-22 16:14:06 +0300

    Add files via upload

---
+++ CSharp/laba 3/Man.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_3
+{
+    class Man
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public Man[] data;
+        // Конструкторы
+        public Man()
+        {
+            Name = "Polina";
+            Age = 18;
+            Country = "Belarus";
+        }
+        public Man(int number)
+        {
+            data = new Man[number];
+        }
+        public Man(string name, int age, string country)
+        {
+            Age = age;
+            Name = name;
+            Country = country;
+        }
+        // Индексатор
+        public Man this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+            set
+            {
+                data[index] = value;
+            }
+        }
+        // Методы
+        public void Print(int i)
+        {
+            Console.WriteLine($"{i}. Name: {Name}, Age: {Age}, Country: {Country}\n");
+        }
+    }
+}
