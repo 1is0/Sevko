3c9a8bebafdfef867f442af88ac597d2d5be8795
polinasevko <61377437+polinasevko@users.noreply.github.com>
2020-04-25 01:11:04 +0300

    Add files via upload

---
+++ CSharp/laba 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Man man = new Man(2);
+            man[0] = new Man("Anastasia", 20, "Belarus");
+            man[1] = new Man();
+            for (int i = 0; i < 2; i++)
+            {
+                man[i].Print(i + 1);
+            }
+        }
+    }
+}
