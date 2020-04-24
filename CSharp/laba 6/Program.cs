a6d649838091c7f5046302b5f0a1fb5d380a34f9
polinasevko <61377437+polinasevko@users.noreply.github.com>
2020-04-25 01:06:16 +0300

    Add files via upload

---
+++ CSharp/laba 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            Specialty[] spec = new Specialty[n];
+            spec[0] = new Specialty("Misha", 19, "Belarus", "BSUIR", Course.First, "IaTP", 75, 60, 4, 5, 5);
+            spec[1] = new Specialty("Inna", 22, "Belarus", "MSLU", Course.Fourth, "MFL", 30, 0, 2, 10, 7);
+            spec[2] = new Specialty();
+            Array.Sort(spec);
+            for (int i = 0; i < n; i++)
+            {
+                spec[i].Print(i + 1);
+            }
+        }
+    }
+}
---
+++ CSharp/laba 6/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_6
+{
+    enum Course
+    {
+        First = 1,
+        Second,
+        Third,
+        Fourth,
+        Fifth
+    }
+    class Student : Man
+    {
+        public string University { get; set; }
+        public Course Cours { get; set; }
+        public Student() : base()
+        {
+            University = "BSUIR";
+            Cours = Course.First;
+        }
+        public Student(string name, int age, string country, string university, Course cor) : base(name, age, country)
+        {
+            University = university;
+            Cours = cor;
+        }
+        public override void Print(int i)
+        {
+            Console.WriteLine($"{i}. Name: {Name}, Age: {Age}, Country: {Country}\n" +
+           $"University: {University}, Course: {(int)Cours}\n");
+        }
+    }
+}
