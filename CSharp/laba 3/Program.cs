6b6d8f3815d7b10b9b56fa9f5692b0ca983ca158
polinasevko <61377437+polinasevko@users.noreply.github.com>
2020-05-18 17:56:44 +0300

    Update Program.cs

--- CSharp/laba 3/Program.cs
+++ CSharp/laba 3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +10,32 @@ namespace laba_3
     {
         static void Main(string[] args)
         {
-            Man man = new Man(2);
+            Man man = new Man(3);
             man[0] = new Man("Anastasia", 20, "Belarus");
             man[1] = new Man();
-            for (int i = 0; i < 2; i++)
+            man[2] = Input();
+            for (int i = 0; i < 3; i++)
             {
                 man[i].Print(i + 1);
             }
+        }
+        static Man Input()
+        {
+            string name;
+            string country;
+            string age;
+            Console.WriteLine("Enter your name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your age:");
+            age = Console.ReadLine();
+            Console.WriteLine("Enter your country:");
+            country = Console.ReadLine();
+            if(!Int32.TryParse(age, out int intAge) || name.Any(x => !char.IsLetter(x)) || country.Any(x => !char.IsLetter(x)))
+            {
+                Console.WriteLine("Wrong input.");
+                Environment.Exit(0);
+            }
+            return new Man(name, intAge, country);
         }
     }
 }
