effb96993b8c6f69a24253859f9e3f9d6c16ddbb
polinasevko <61377437+polinasevko@users.noreply.github.com>
2020-05-18 15:33:26 +0300

    Update Program.cs

--- CSharp/laba 5/Program.cs
+++ CSharp/laba 5/Program.cs
@@ -10,14 +10,6 @@ namespace laba_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("STUDENTS");
-            Student stud = new Student(2);
-            stud[0] = new Student();
-            stud[1] = new Student("Roman", 19, "Belarus", "BSMU", Course.Second);
-            for (int i = 0; i < 2; i++)
-            {
-                stud[i].Print(i + 1);
-            }
             Console.WriteLine("STUDENTS OF DIFFERENT SPECIALTIES");
             Specialty spec = new Specialty(2);
             spec[0] = new Specialty();
@@ -29,4 +21,3 @@ namespace laba_5
         }
     }
 }
-
