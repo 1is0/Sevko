c52f4bd1d9632d050379527444797a7a346425a8
polinasevko <61377437+polinasevko@users.noreply.github.com>
2020-05-18 15:52:38 +0300

    Update Student.cs

--- CSharp/laba 6/Student.cs
+++ CSharp/laba 6/Student.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +14,7 @@ namespace laba_6
         Fourth,
         Fifth
     }
-    class Student : Man
+    abstract class Student : Man
     {
         public string University { get; set; }
         public Course Cours { get; set; }
@@ -23,22 +23,10 @@ namespace laba_6
             University = "BSUIR";
             Cours = Course.First;
         }
-        public Student(int number)
-        {
-            data = new Student[number];
-        }
         public Student(string name, int age, string country, string university, Course cor) : base(name, age, country)
         {
             University = university;
             Cours = cor;
-        }
-        public override void Print(int i)
-        {
-            Console.WriteLine($"{i}. Name: {Name}\n" +
-               $"Age: {Age}\n" +
-               $"Country: {Country}\n" +
-               $"University: {University}\n" +
-               $"Course: {(int)Cours}\n");
         }
     }
 }
